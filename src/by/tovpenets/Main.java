06b3e0c36032789e1ed76196d10361444058c768
ilya1301 <ilya.tovpenec!mail.ru>
2022-02-17 22:27:09 +0300

    Initial commit

---
+++ src/by/tovpenets/Main.java
@@ -0,0 +1,23 @@
+package by.tovpenets;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        byte  byteVar =127;
+        long longVar =100;
+        char charVar ='a';
+        double doubleVar=2.9;
+        boolean bootVar =true;
+        int intVar = byteVar;
+        int intVar2 =(int) longVar;
+
+        int a =10;
+        int b =12;
+        System.out.println("a + b = " + (a + b));
+        System.out.println( "a - b = "+ (a - b));
+        System.out.println( "a * b = " + (a * b));
+
+    }
+}
