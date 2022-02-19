cf3d42f6a3ca146a032b21ba3867cf9672a020fb
ilya1301 <ilya.tovpenec!mail.ru>
2022-02-19 15:09:31 +0300

    домашнее задание Scanner

--- src/by/tovpenets/Main.java
+++ src/by/tovpenets/Main.java
@@ -1,23 +1,36 @@
 package by.tovpenets;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        byte  byteVar =127;
-        long longVar =100;
-        char charVar ='a';
-        double doubleVar=2.9;
-        boolean bootVar =true;
-        int intVar = byteVar;
-        int intVar2 =(int) longVar;
 
-        int a =10;
-        int b =12;
-        System.out.println("a + b = " + (a + b));
-        System.out.println( "a - b = "+ (a - b));
-        System.out.println( "a * b = " + (a * b));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("привет, как тебя зовут?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("приятно познакомиться, " + name);
+
+
+
+
+//        System.out.println("hello world");
+//        byte  byteVar =127;
+//        long longVar =100;
+//        char charVar ='a';
+//        double doubleVar=2.9;
+//        boolean bootVar =true;
+//        int intVar = byteVar;
+//        int intVar2 =(int) longVar;
+//
+//        int a =10;
+//        int b =12;
+//        System.out.println("a + b = " + (a + b));
+//        System.out.println( "a - b = "+ (a - b));
+//        System.out.println( "a * b = " + (a * b));
 
     }
 }
