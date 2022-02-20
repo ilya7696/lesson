592ec80be86615c980baeb3cba206f2e677bca64
ilya1301 <ilya.tovpenec!mail.ru>
2022-02-20 13:22:13 +0300

    домашнее задание Scanner

--- src/by/tovpenets/Main.java
+++ src/by/tovpenets/Main.java
@@ -17,20 +17,49 @@ public class Main {
 
 
 
-//        System.out.println("hello world");
-//        byte  byteVar =127;
-//        long longVar =100;
-//        char charVar ='a';
-//        double doubleVar=2.9;
-//        boolean bootVar =true;
-//        int intVar = byteVar;
-//        int intVar2 =(int) longVar;
-//
-//        int a =10;
-//        int b =12;
-//        System.out.println("a + b = " + (a + b));
-//        System.out.println( "a - b = "+ (a - b));
-//        System.out.println( "a * b = " + (a * b));
+        System.out.println("hello world");
+        byte  byteVar =127;
+        long longVar =100;
+        char charVar ='a';
+        double doubleVar=2.9;
+        boolean bootVar =true;
+        int intVar = byteVar;
+        int intVar2 =(int) longVar;
 
-    }
+        int a =10;
+        int b =12;
+        System.out.println("a + b = " + (a + b));
+        System.out.println( "a - b = "+ (a - b));
+        System.out.println( "a * b = " + (a * b));
+
+
+        System.out.println("1 & 1 = "+ (1 & 1));
+        System.out.println("1 | 1 = " + (1 | 2));
+        System.out.println("1 ^ 1 = " + (1 ^ 2));
+        System.out.println(" ~1 " + (~1));
+
+        System.out.println((1 == 1) && (2 == 2));
+        System.out.println((2 == 1) && (1 == 1));
+        System.out.println((1 == 1) || (2 == 2));
+        System.out.println((2 == 1) || (2 == 1));
+
+        System.out.println(!(1 == 1));
+        System.out.println(!(1 == 2));
+
+        int c = 10;
+        c += 5;
+        System.out.println(c);
+        c -= 3;
+        System.out.println(c);
+        c *= 2;
+        System.out.println(c);
+        c /= 2;
+        System.out.println(c);
+
+        int d = (c < a) ? 1 : 0;
+        System.out.println(d);
+
+
+
+        }
 }
