00cd6f300d81ac71c9c2f51fc8defc40857d00f7
ilya1301 <ilya.tovpenec!mail.ru>
2022-02-24 20:10:59 +0300

    домашнее задание Scanner 1

--- src/by/tovpenets/Main.java
+++ src/by/tovpenets/Main.java
@@ -1,6 +1,7 @@
 package by.tovpenets;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -15,25 +16,23 @@ public class Main {
         System.out.println("приятно познакомиться, " + name);
 
 
-
-
         System.out.println("hello world");
-        byte  byteVar =127;
-        long longVar =100;
-        char charVar ='a';
-        double doubleVar=2.9;
-        boolean bootVar =true;
+        byte byteVar = 127;
+        long longVar = 100;
+        char charVar = 'a';
+        double doubleVar = 2.9;
+        boolean bootVar = true;
         int intVar = byteVar;
-        int intVar2 =(int) longVar;
+        int intVar2 = (int) longVar;
 
-        int a =10;
-        int b =12;
+        int a = 10;
+        int b = 12;
         System.out.println("a + b = " + (a + b));
-        System.out.println( "a - b = "+ (a - b));
-        System.out.println( "a * b = " + (a * b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
 
 
-        System.out.println("1 & 1 = "+ (1 & 1));
+        System.out.println("1 & 1 = " + (1 & 1));
         System.out.println("1 | 1 = " + (1 | 2));
         System.out.println("1 ^ 1 = " + (1 ^ 2));
         System.out.println(" ~1 " + (~1));
@@ -61,5 +60,64 @@ public class Main {
 
 
 
-        }
+
+    }
+
+  //      1. написать игру камень ножницы бумага.
+/*                0.бесконечный цикл.
+                1. сопостовляем значение к н б .
+                2. сказать игроку порядок действий.
+                3. значение переменной из ввода.
+                4. рандом.
+                5. проверить правильность ввода числа.
+                6. проверка выхода из программы.
+                7. сама игра.*/
+
+
+
+    public static void main(String[] args){
+    int[] digits = {0, 1, 4, 3, 10, -1};
+        System.out.println(Arrays.toString(digits);
+        Arrays.toString(digits);
+        System.out.println(Arrays.toString(digits));
+    int position = Arrays.toString(digits, key: 10);
+        System.out.println(digits[position] == 10);
+    int[] destinationArray = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,};
+        System.arraycopy(digits, srcPos: 1, destinationArray, destPost: 2, );
+        System.out.println(Arrays . tos4String(destinationArray));
+
+
+    int[] swap ={2, 1};
+    int temp;
+        System.out.println(ArrayString(swap));
+    temp = swap[0];
+    swap[1] = swap[0];
+    swap[0] = temp;
+        System.out.println(Arrays.toString(swap));
+
+
+    int func(){
+        return func();
+
+
+        int[] quicksotrt(int[] arr. int low, int high){
+            if(arr.length <2){
+                return arr;
+            }
+                ...
+            quicksort(arr1,_);
+            quicksotrt(arr2,_);
+            }
+
+
+    }
+
+        {
+
+
+
+
+
+
+    }
 }
